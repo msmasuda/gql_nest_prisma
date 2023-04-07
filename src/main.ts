20266ca9a27a02c089653840266b4d3e501738bc
Shinichi Masuda <masuda@my-style.jp>
2023-04-07 02:25:43 +0000

    初期コミット

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { PrismaService } from './prisma.service';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+  await app.listen(3000);
+}
+bootstrap();
